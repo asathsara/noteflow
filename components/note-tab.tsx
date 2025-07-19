f48c57f947be692c917b7e371cb96325ae0ae48f
A S <65203960+asathsara@users.noreply.github.com>
2025-07-19 21:47:17 +0530

    implement formatting using marked for gemini response

--- components/note-tab.tsx
+++ components/note-tab.tsx
@@ -1,27 +1,27 @@
 "use client"
 
-
 import Tiptap from "./tip-tap"
 import { AskAIDialog } from "./ask-ai-dialog"
-import { Button } from "./ui/button"
+import { convertMarkdownToHTML } from "@/lib/utils"
 
 interface NoteTabProps {
     note: string
     setNote: (value: string) => void
     onTriggerAI: () => void
+
 }
 
 export function NoteTab({ note, setNote }: NoteTabProps) {
-    const handleAIResponse = (response: string) => {
-        const newHtml = `${note}<p>${response}</p>`
-        setNote(newHtml)
+    const  handleAIResponse = async (response: string) => {
+        const html =  await convertMarkdownToHTML(response)
+        setNote(`${note}<hr />${html}`)
     }
 
     return (
         <>
             <Tiptap note={note} setNote={setNote} />
             <AskAIDialog onAnswer={handleAIResponse} />
-            
+
         </>
     )
 }
