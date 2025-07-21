a3599d2d3acd1d1e0504b2ab2a547f45fcc70924
A S <65203960+asathsara@users.noreply.github.com>
2025-07-21 23:31:37 +0530

    implement clerk

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,14 +2,40 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { ReactNode } from "react";
 
+import {
+    SignInButton,
+    SignUpButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+} from '@clerk/nextjs'
+
 export default function MainLayout({ children }: { children: ReactNode }) {
     return (
+
+
         <SidebarProvider>
-                <AppSidebar />
-                <main className="flex-1 bg-muted/30 py-12 px-4 sm:px-6 lg:px-8">
-                    <SidebarTrigger />
-                    {children}
-                </main>
+            <AppSidebar />
+
+            <main className="flex-1 bg-muted/30 px-4 sm:px-6 lg:px-8">
+
+                <header className="flex justify-end items-center p-4 gap-4 h-16">
+                    <SignedOut>
+                        <SignInButton />
+                        <SignUpButton>
+                            <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                                Sign Up
+                            </button>
+                        </SignUpButton>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                </header>
+                <SidebarTrigger />
+
+                {children}
+            </main>
         </SidebarProvider>
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 
+import {
+  ClerkProvider,
+} from '@clerk/nextjs'
+
 const inter = Inter({
   subsets: ["latin"],
 });
@@ -19,18 +23,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={inter.className}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={inter.className}
         >
-          {children}</ThemeProvider>
-      </body>
-    </html>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            
+            {children}</ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
