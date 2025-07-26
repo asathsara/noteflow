874d98625977bdb61abb038fc0533b4b195cf982
A S <65203960+asathsara@users.noreply.github.com>
2025-07-26 11:21:41 +0530

    add  loading state to editor

--- components/rich-text-editor/index.tsx
+++ components/rich-text-editor/index.tsx
@@ -1,13 +1,15 @@
+"use client"
+
 import { EditorContent, useEditor } from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
 import MenuBar from "./menu-bar"
 import { useEffect } from "react"
+import { Skeleton } from "../ui/skeleton"
 
 interface RichTextEditorProps {
   note: string
   setNote: (value: string) => void
 }
-
 
 export default function RichTextEditor({ note, setNote }: RichTextEditorProps) {
   const editor = useEditor({
@@ -24,28 +26,35 @@ export default function RichTextEditor({ note, setNote }: RichTextEditorProps) {
         HTMLAttributes: {
           class: "list-decimal ml-6",
         },
-        
       },
     })],
     content: note || '<p>Start writing your note here</p>',
     editorProps: {
       attributes: {
-        class: "rounded-md border min-h-[200px] p-4",
+        class: "rounded-md border min-h-[200px] p-4  focus:outline-none focus:ring-3 focus:ring-neutral-800",
       },
     },
     onUpdate: ({ editor }) => {
-      // console.log(editor.getHTML());
       setNote(editor.getHTML());
     },
-    // Don't render immediately on the server to avoid SSR issues
     immediatelyRender: false,
   })
 
   useEffect(() => {
     if (editor && note !== editor.getHTML()) {
-      editor.commands.setContent(note);
+      editor.commands.setContent(note)
     }
   }, [note, editor])
+
+  
+  if (!editor) {
+    return (
+      <div className="space-y-4 mt-4">
+        <Skeleton className="h-8 w-32" />
+        <Skeleton className="h-[200px] w-full rounded-md" />
+      </div>
+    )
+  }
 
   return (
     <>
