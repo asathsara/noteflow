191ac9d1dceafe3f4e935691335a2028c1b42000
A S <65203960+asathsara@users.noreply.github.com>
2025-07-26 08:55:26 +0530

    add NoNotebooks component  to handle empty state

--- components/note-table.tsx
+++ components/note-table.tsx
@@ -14,6 +14,7 @@ import Link from "next/link"
 import { DeleteAlertDialog } from "./delete-dialog"
 import { useNotebooks } from "@/context/notes-context"
 import { LoadingTable } from "./loading-table"
+import { NoNotebooks } from "./no-notebook"
 
 export function NoteTable() {
   const { notebooks, deleteById, loading } = useNotebooks()
@@ -24,7 +25,7 @@ export function NoteTable() {
   }
 
   if (notebooks.length === 0) {
-    return <p className="text-center text-gray-500">No notes yet.</p>
+    return <NoNotebooks />
   }
 
   return (
